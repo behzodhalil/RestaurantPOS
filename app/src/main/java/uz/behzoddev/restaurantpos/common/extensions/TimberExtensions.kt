3827aebb9242f19788ac9ff532558e741eef7b75
behzod <behzoddev@gmail.com>
2022-02-15 16:48:55 +0900

    Update timber extensions

--- app/src/main/java/uz/behzoddev/restaurantpos/common/extensions/TimberExtensions.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/common/extensions/TimberExtensions.kt
@@ -1,7 +1,14 @@
 package uz.behzoddev.restaurantpos.common.extensions
 
 import timber.log.Timber
+import kotlin.math.log
 
-fun debugging(debug: String) {
-    return Timber.d(debug)
-}
+/**
+ * Extension function that wraps Timber's debug logging
+ */
+internal fun debug(message:String) { Timber.d(message) }
+
+/**
+ * Extension function that wraps Timber's info logging
+ */
+internal fun info(message: String) { Timber.i(message)}
