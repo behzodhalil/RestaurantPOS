76c52721dc88353a9a350c9a2c289e47e758a432
behzod <behzoddev@gmail.com>
2022-02-15 12:07:25 +0900

    Update MainActivity.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/MainActivity.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/MainActivity.kt
@@ -1,7 +1,7 @@
 package uz.behzoddev.restaurantpos
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
