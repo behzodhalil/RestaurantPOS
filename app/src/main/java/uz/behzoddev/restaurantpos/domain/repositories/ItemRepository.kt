98bbc005fe613151e4c0654cc07f0d144e0e597e
behzod <behzoddev@gmail.com>
2022-02-15 15:58:36 +0900

    Move repositories to domain

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/repositories/ItemRepository.kt
@@ -0,0 +1,4 @@
+package uz.behzoddev.restaurantpos.domain.repositories
+
+interface ItemRepository {
+}
--- app/src/main/java/uz/behzoddev/restaurantpos/presentation/repositories/ItemRepository.kt
+++
@@ -1,4 +0,0 @@
-package uz.behzoddev.restaurantpos.presentation.repositories
-
-interface ItemRepository {
-}
