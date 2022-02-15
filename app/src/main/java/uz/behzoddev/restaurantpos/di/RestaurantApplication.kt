e1e1592da860ad509514a5975d185c91255b7915
behzod <behzoddev@gmail.com>
2022-02-15 16:57:20 +0900

    Update application class

--- app/src/main/java/uz/behzoddev/restaurantpos/di/RestaurantApplication.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/di/RestaurantApplication.kt
@@ -1,12 +1,35 @@
 package uz.behzoddev.restaurantpos.di
 
 import android.app.Application
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.Logger
+import com.orhanobut.logger.PrettyFormatStrategy
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+import uz.behzoddev.restaurantpos.common.extensions.debug
 
 @HiltAndroidApp
 class RestaurantApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
+            .showThreadInfo(true)
+            .methodCount(1)
+            .methodOffset(5)
+            .tag("")
+            .build()
+
+        Logger.addLogAdapter(AndroidLogAdapter(formatStrategy))
+
+        Timber.plant(object : Timber.DebugTree() {
+            override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+                Logger.log(priority,"-$tag",message,t)
+            }
+        })
+
+        debug("onCreate: Inside Application")
     }
 }
