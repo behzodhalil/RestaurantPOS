e04855850093f0af1f9e763f4e9de806559c9a91
behzod <behzoddev@gmail.com>
2022-02-15 12:05:04 +0900

    Add room contracts

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/common/local/RoomContract.kt
@@ -0,0 +1,22 @@
+package uz.behzoddev.restaurantpos.common.local
+
+object RoomContract {
+    object FoodItem {
+        const val FOOD_ITEM_DATABASE = "food_item_db"
+        const val FOOD_ITEM_TABLE = "food_item_table"
+        const val FOOD_ITEM_ID = "food_item_id"
+        const val FOOD_ITEM_NAME = "food_item_name"
+        const val FOOD_ITEM_PRICE = "food_item_price"
+        const val FOOD_ITEM_SUB_COUNT = "food_item_sub_count"
+        const val FOOD_ITEM_TOTAL_COUNT = "food_item_total_count"
+        const val FOOD_ITEM_CATEGORY = "food_item_category"
+        const val FOOD_ITEM_PHOTO = "food_item_photo"
+        const val FOOD_ITEM_PAID = "food_item_is_paid"
+    }
+    object FoodMenu {
+
+    }
+    object FoodStore {
+
+    }
+}
