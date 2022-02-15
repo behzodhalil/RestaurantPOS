d80534d05a255d37719c1dd01ac322dd5b36e021
behzod <behzoddev@gmail.com>
2022-02-15 16:57:33 +0900

    Update main activity

--- app/src/main/java/uz/behzoddev/restaurantpos/view/MainActivity.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/MainActivity.kt
@@ -2,11 +2,14 @@ package uz.behzoddev.restaurantpos.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 import uz.behzoddev.restaurantpos.R
+import uz.behzoddev.restaurantpos.common.extensions.debug
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.d("onCreate(): Inside Main Activity")
     }
 }
