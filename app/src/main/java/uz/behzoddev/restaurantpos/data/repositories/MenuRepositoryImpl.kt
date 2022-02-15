b2e39ad2359e67bb09902d185d88f66cc9bfe002
behzod <behzoddev@gmail.com>
2022-02-15 15:56:02 +0900

    Add repositories

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/repositories/MenuRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package uz.behzoddev.restaurantpos.data.repositories
+
+class MenuRepositoryImpl {
+}
---
+++ app/src/main/java/uz/behzoddev/restaurantpos/presentation/repositories/MenuRepository.kt
@@ -0,0 +1,4 @@
+package uz.behzoddev.restaurantpos.presentation.repositories
+
+interface MenuRepository {
+}
