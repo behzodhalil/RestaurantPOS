6e967abf21dfae8560f236666f9184717ef3313e
behzod <behzoddev@gmail.com>
2022-02-16 10:57:23 +0900

    Update dashboard

--- app/src/main/java/uz/behzoddev/restaurantpos/view/main/DashboardFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/main/DashboardFragment.kt
@@ -6,17 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Spinner
 import uz.behzoddev.restaurantpos.common.base.BaseFragment
-import uz.behzoddev.restaurantpos.databinding.FragmentMainBinding
+import uz.behzoddev.restaurantpos.databinding.FragmentDashboardBinding
 
-class MainFragment : BaseFragment<FragmentMainBinding>() {
+class DashboardFragment : BaseFragment<FragmentDashboardBinding>() {
 
     private lateinit var languageSpinner: Spinner
 
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentMainBinding {
-        return FragmentMainBinding.inflate(inflater,container,false)
+    ): FragmentDashboardBinding {
+        return FragmentDashboardBinding.inflate(inflater,container,false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
