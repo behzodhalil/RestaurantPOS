075ec0d1f9a637243e9603ff7f2f55f3456502e1
behzod <behzoddev@gmail.com>
2022-02-16 16:51:45 +0900

    Add data source module

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/di/modules/DataSourceModule.kt
@@ -0,0 +1,35 @@
+package uz.behzoddev.restaurantpos.di.modules
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import uz.behzoddev.restaurantpos.data.local.datasource.*
+import uz.behzoddev.restaurantpos.di.annotations.DatasourceBinds
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+    @Binds
+    @DatasourceBinds
+    @Singleton
+    abstract fun provideItemDatasourceInstance(
+        itemDataSourceImpl: ItemDataSourceImpl
+    ): ItemDataSource
+
+    @Binds
+    @DatasourceBinds
+    @Singleton
+    abstract fun provideMenuDatasourceInstance(
+        menuDataSourceImpl: MenuDataSourceImpl
+    ): MenuDataSource
+
+    @Binds
+    @DatasourceBinds
+    @Singleton
+    abstract fun provideStoreDatasourceInstance(
+        storeDataSourceImpl: StoreDataSourceImpl
+    ): StoreDataSource
+}
