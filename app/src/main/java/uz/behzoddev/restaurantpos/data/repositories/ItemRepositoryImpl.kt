426a5fec2beff0a395e7f541051790b992b73092
behzod <behzoddev@gmail.com>
2022-02-16 16:50:58 +0900

    Update item repository

--- app/src/main/java/uz/behzoddev/restaurantpos/data/repositories/ItemRepositoryImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/repositories/ItemRepositoryImpl.kt
@@ -1,4 +1,33 @@
 package uz.behzoddev.restaurantpos.data.repositories
 
-class ItemRepositoryImpl {
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.datasource.ItemDataSource
+import uz.behzoddev.restaurantpos.data.local.models.FoodItem
+import uz.behzoddev.restaurantpos.di.annotations.DatasourceBinds
+import uz.behzoddev.restaurantpos.domain.repositories.ItemRepository
+import javax.inject.Inject
+
+class ItemRepositoryImpl @Inject constructor(
+    @DatasourceBinds private val itemDataSource: ItemDataSource
+) : ItemRepository {
+    override suspend fun insertFoodItem(foodItem: FoodItem): Long {
+        return itemDataSource.insertFoodItem(foodItem)
+    }
+
+    override suspend fun updateFoodItem(foodItem: FoodItem): Long {
+        return itemDataSource.updateFoodItem(foodItem)
+    }
+
+    override suspend fun deleteFoodItem(foodItem: FoodItem): Long {
+        return itemDataSource.deleteFoodItem(foodItem)
+    }
+
+    override fun fetchAllFoodItems(): Flow<List<FoodItem>> {
+        return itemDataSource.fetchAllFoodItems()
+    }
+
+    override fun fetchAllFoodItemsByName(): Flow<List<FoodItem>> {
+        return itemDataSource.fetchAllFoodItemsByName()
+    }
+
 }
--- app/src/main/java/uz/behzoddev/restaurantpos/domain/repositories/ItemRepository.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/repositories/ItemRepository.kt
@@ -1,4 +1,12 @@
 package uz.behzoddev.restaurantpos.domain.repositories
 
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.models.FoodItem
+
 interface ItemRepository {
+    suspend fun insertFoodItem(foodItem: FoodItem): Long
+    suspend fun updateFoodItem(foodItem: FoodItem): Long
+    suspend fun deleteFoodItem(foodItem: FoodItem): Long
+    fun fetchAllFoodItems(): Flow<List<FoodItem>>
+    fun fetchAllFoodItemsByName(): Flow<List<FoodItem>>
 }
