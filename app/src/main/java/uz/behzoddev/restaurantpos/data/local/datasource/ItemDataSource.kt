82466a378dd0bbc19fd5be936fe14a3d710767b7
behzod <behzoddev@gmail.com>
2022-02-16 16:19:14 +0900

    Update item data source

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/datasource/ItemDataSource.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/datasource/ItemDataSource.kt
@@ -1,4 +1,12 @@
 package uz.behzoddev.restaurantpos.data.local.datasource
 
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.models.FoodItem
+
 interface ItemDataSource {
+    suspend fun insertFoodItem(foodItem: FoodItem): Long
+    suspend fun updateFoodItem(foodItem: FoodItem): Long
+    suspend fun deleteFoodItem(foodItem: FoodItem): Long
+    fun fetchAllFoodItems(): Flow<List<FoodItem>>
+    fun fetchAllFoodItemsByName(): Flow<List<FoodItem>>
 }
---
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/datasource/ItemDataSourceImpl.kt
@@ -0,0 +1,30 @@
+package uz.behzoddev.restaurantpos.data.local.datasource
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.dao.FoodItemDao
+import uz.behzoddev.restaurantpos.data.local.models.FoodItem
+import javax.inject.Inject
+
+class ItemDataSourceImpl @Inject constructor(
+    private val itemDao: FoodItemDao
+) : ItemDataSource {
+    override suspend fun insertFoodItem(foodItem: FoodItem): Long {
+        return itemDao.insertFoodItem(foodItem)
+    }
+
+    override suspend fun updateFoodItem(foodItem: FoodItem): Long {
+        return itemDao.updateFoodItem(foodItem)
+    }
+
+    override suspend fun deleteFoodItem(foodItem: FoodItem): Long {
+        return itemDao.deleteFoodItem(foodItem)
+    }
+
+    override fun fetchAllFoodItems(): Flow<List<FoodItem>> {
+        return itemDao.fetchAllFoodItems()
+    }
+
+    override fun fetchAllFoodItemsByName(): Flow<List<FoodItem>> {
+        return itemDao.fetchAllFoodItemsByName()
+    }
+}
