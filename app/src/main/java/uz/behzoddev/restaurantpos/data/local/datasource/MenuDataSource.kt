57eeafd779e23e037510258bf0f9df19c5aede2a
behzod <behzoddev@gmail.com>
2022-02-16 16:19:02 +0900

    Update menu data source

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/datasource/MenuDataSource.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/datasource/MenuDataSource.kt
@@ -1,4 +1,11 @@
 package uz.behzoddev.restaurantpos.data.local.datasource
 
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
+
 interface MenuDataSource {
+    suspend fun insertMenu(foodMenu: FoodMenu): Long
+    suspend fun updateMenu(foodMenu: FoodMenu): Long
+    suspend fun deleteMenu(foodMenu: FoodMenu): Long
+    fun fetchAllMenus(): Flow<List<FoodMenu>>
 }
