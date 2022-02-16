71cf1373e607e0c94075cedd64b041237c15da00
behzod <behzoddev@gmail.com>
2022-02-16 16:51:17 +0900

    Update store repository

--- app/src/main/java/uz/behzoddev/restaurantpos/data/repositories/StoreRepositoryImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/repositories/StoreRepositoryImpl.kt
@@ -1,4 +1,33 @@
 package uz.behzoddev.restaurantpos.data.repositories
 
-class StoreRepositoryImpl {
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.datasource.StoreDataSource
+import uz.behzoddev.restaurantpos.data.local.models.FoodStore
+import uz.behzoddev.restaurantpos.di.annotations.DatasourceBinds
+import uz.behzoddev.restaurantpos.domain.repositories.StoreRepository
+import javax.inject.Inject
+
+class StoreRepositoryImpl @Inject constructor(
+    @DatasourceBinds private val storeDataSource: StoreDataSource
+) : StoreRepository {
+
+    override suspend fun insertStore(foodStore: FoodStore): Long {
+        return storeDataSource.insertStore(foodStore)
+    }
+
+    override suspend fun updateStore(foodStore: FoodStore): Long {
+        return storeDataSource.updateStore(foodStore)
+    }
+
+    override suspend fun deleteStore(foodStore: FoodStore): Long {
+        return storeDataSource.deleteStore(foodStore)
+    }
+
+    override fun fetchAllStores(): Flow<List<FoodStore>> {
+        return storeDataSource.fetchAllStores()
+    }
+
+    override fun fetchAllStoresById(foodStoreId: Int): Flow<FoodStore> {
+        return storeDataSource.fetchAllStoresById(foodStoreId)
+    }
 }
--- app/src/main/java/uz/behzoddev/restaurantpos/domain/repositories/StoreRepository.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/repositories/StoreRepository.kt
@@ -1,4 +1,12 @@
 package uz.behzoddev.restaurantpos.domain.repositories
 
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.models.FoodStore
+
 interface StoreRepository {
+    suspend fun insertStore(foodStore: FoodStore): Long
+    suspend fun updateStore(foodStore: FoodStore): Long
+    suspend fun deleteStore(foodStore: FoodStore): Long
+    fun fetchAllStores(): Flow<List<FoodStore>>
+    fun fetchAllStoresById(foodStoreId: Int): Flow<FoodStore>
 }
