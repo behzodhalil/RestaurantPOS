bb582ff6f4bdc373ad7b2867dfb1bc292a15b9d4
behzod <behzoddev@gmail.com>
2022-02-16 16:51:07 +0900

    Update menu repository

--- app/src/main/java/uz/behzoddev/restaurantpos/data/repositories/MenuRepositoryImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/repositories/MenuRepositoryImpl.kt
@@ -1,4 +1,28 @@
 package uz.behzoddev.restaurantpos.data.repositories
 
-class MenuRepositoryImpl {
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.datasource.MenuDataSource
+import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
+import uz.behzoddev.restaurantpos.di.annotations.DatasourceBinds
+import uz.behzoddev.restaurantpos.domain.repositories.MenuRepository
+import javax.inject.Inject
+
+class MenuRepositoryImpl @Inject constructor(
+    @DatasourceBinds private val menuDataSource: MenuDataSource
+) : MenuRepository {
+    override suspend fun insertMenu(foodMenu: FoodMenu): Long {
+        return menuDataSource.insertMenu(foodMenu)
+    }
+
+    override suspend fun updateMenu(foodMenu: FoodMenu): Long {
+        return menuDataSource.updateMenu(foodMenu)
+    }
+
+    override suspend fun deleteMenu(foodMenu: FoodMenu): Long {
+        return menuDataSource.deleteMenu(foodMenu)
+    }
+
+    override fun fetchAllMenus(): Flow<List<FoodMenu>> {
+        return menuDataSource.fetchAllMenus()
+    }
 }
--- app/src/main/java/uz/behzoddev/restaurantpos/domain/repositories/MenuRepository.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/repositories/MenuRepository.kt
@@ -1,4 +1,11 @@
 package uz.behzoddev.restaurantpos.domain.repositories
 
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
+
 interface MenuRepository {
+    suspend fun insertMenu(foodMenu: FoodMenu): Long
+    suspend fun updateMenu(foodMenu: FoodMenu): Long
+    suspend fun deleteMenu(foodMenu: FoodMenu): Long
+    fun fetchAllMenus(): Flow<List<FoodMenu>>
 }
