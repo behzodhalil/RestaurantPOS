50b49a8036d31b24e9d59a4e96e6864146a557f9
behzod <behzoddev@gmail.com>
2022-02-16 13:25:37 +0900

    Add local database module

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/di/LocalDatabaseModule.kt
@@ -0,0 +1,12 @@
+package uz.behzoddev.restaurantpos.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalDatabaseModule {
+
+
+}
