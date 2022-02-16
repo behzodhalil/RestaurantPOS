b6d80ba45ceb91a1e72d170577be7f14f1586a33
behzod <behzoddev@gmail.com>
2022-02-16 15:39:18 +0900

    Update timber extension

--- app/src/main/java/uz/behzoddev/restaurantpos/common/extensions/TimberExtensions.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/common/extensions/TimberExtensions.kt
@@ -3,17 +3,32 @@ package uz.behzoddev.restaurantpos.common.extensions
 import timber.log.Timber
 import kotlin.math.log
 
-// https://github.com/ajalt/timberkt
+/**
+ * This extensions builds on Timber with an API that's easier to use from Kotlin.
+ * Instead of using formatting parameters, you pass a lambda that is only evaluated
+ * if the message is logged.
+ */
 
 /**
  * Extension function that wraps Timber's debug logging
  */
-inline fun debug(t: Throwable? = null,message: () -> String) = log { Timber.d(t,message()) }
+inline fun debug(t: Throwable? = null,message: () -> String) {
+    return log { Timber.d(t,message())}
+}
 
 /**
  * Extension function that wraps Timber's info logging
  */
-inline fun info(message: () -> String) = log { Timber.i(message()) }
+inline fun info(message: () -> String) {
+    return log { Timber.i(message()) }
+}
+
+/**
+ * Extension function that wraps Timber's error logging
+ */
+inline fun error(t: Throwable? = null, message: () -> String) {
+    return log { Timber.e(t,message()) }
+}
 
 
 @PublishedApi
