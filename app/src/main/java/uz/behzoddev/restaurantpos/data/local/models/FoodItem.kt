01681e870a972647e4937ec964685ae20fa4d3ce
behzod <behzoddev@gmail.com>
2022-02-16 16:17:28 +0900

    Update data classes

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodItem.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodItem.kt
@@ -8,23 +8,23 @@ import kotlinx.parcelize.Parcelize
 import uz.behzoddev.restaurantpos.common.local.RoomContract
 
 @Parcelize
-@Entity(tableName = RoomContract.FoodItem.FOOD_ITEM_TABLE)
+@Entity(tableName = RoomContract.FoodItemContract.FOOD_ITEM_TABLE)
 data class FoodItem(
-    @ColumnInfo(name = RoomContract.FoodItem.FOOD_ITEM_NAME)
+    @ColumnInfo(name = RoomContract.FoodItemContract.FOOD_ITEM_NAME)
     val foodItemName : String,
-    @ColumnInfo(name = RoomContract.FoodItem.FOOD_ITEM_PRICE)
+    @ColumnInfo(name = RoomContract.FoodItemContract.FOOD_ITEM_PRICE)
     val foodItemPrice : Long,
-    @ColumnInfo(name = RoomContract.FoodItem.FOOD_ITEM_SUB_COUNT)
+    @ColumnInfo(name = RoomContract.FoodItemContract.FOOD_ITEM_SUB_COUNT)
     val foodItemSubCount : Int,
-    @ColumnInfo(name = RoomContract.FoodItem.FOOD_ITEM_TOTAL_COUNT)
+    @ColumnInfo(name = RoomContract.FoodItemContract.FOOD_ITEM_TOTAL_COUNT)
     val foodItemTotalCount : Int,
-    @ColumnInfo(name = RoomContract.FoodItem.FOOD_ITEM_CATEGORY)
+    @ColumnInfo(name = RoomContract.FoodItemContract.FOOD_ITEM_CATEGORY)
     val foodItemCategory : String,
-    @ColumnInfo(name = RoomContract.FoodItem.FOOD_ITEM_PHOTO)
+    @ColumnInfo(name = RoomContract.FoodItemContract.FOOD_ITEM_PHOTO)
     val foodItemPhoto : String,
-    @ColumnInfo(name = RoomContract.FoodItem.FOOD_ITEM_PAID)
+    @ColumnInfo(name = RoomContract.FoodItemContract.FOOD_ITEM_PAID)
     var foodItemIsPaid: Boolean = false,
-    @ColumnInfo(name = RoomContract.FoodItem.FOOD_ITEM_ID)
+    @ColumnInfo(name = RoomContract.FoodItemContract.FOOD_ITEM_ID)
     @PrimaryKey(autoGenerate = true)
     var foodItemId : Long = 0
 ) : Parcelable
--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodMenu.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodMenu.kt
@@ -4,10 +4,10 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import uz.behzoddev.restaurantpos.common.local.RoomContract
 
-@Entity(tableName = RoomContract.FoodMenu.FOOD_MENU_TABLE)
+@Entity(tableName = RoomContract.FoodMenuContract.FOOD_MENU_TABLE)
 data class FoodMenu(
-    @ColumnInfo(name = RoomContract.FoodMenu.FOOD_MENU_ID)
+    @ColumnInfo(name = RoomContract.FoodMenuContract.FOOD_MENU_ID)
     val foodMenuId: Int,
-    @ColumnInfo(name = RoomContract.FoodMenu.FOOD_MENU_NAME)
+    @ColumnInfo(name = RoomContract.FoodMenuContract.FOOD_MENU_NAME)
     var foodMenuName: String
 )
