87fecdb26aaa25d0facb99cbb9841fbaeb8e8afc
behzod <behzoddev@gmail.com>
2022-02-17 14:17:55 +0900

    Refactor Groovy to Kotlin DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,14 @@ plugins {
 }
 
 android {
-    compileSdk  = 31
+    compileSdk  = Configs.compileSdkVersion
 
     defaultConfig {
-        applicationId  = "uz.behzoddev.restaurantpos"
-        minSdk = 21
-        targetSdk  = 31
-        versionCode = 1
-        versionName  = "1.0"
+        applicationId = Configs.applicationIdName
+        minSdk = Configs.minSdkVersion
+        targetSdk  = Configs.targetSdkVersion
+        versionCode = Configs.versionCodeVersion
+        versionName  = Configs.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -43,46 +43,22 @@ android {
 
 dependencies {
 
-    implementation ("androidx.core:core-ktx:1.7.0")
-    implementation ("androidx.appcompat:appcompat:1.4.1")
-    implementation ("com.google.android.material:material:1.5.0")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.3")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    // Jetpack Navigation
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.4.1")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.4.1")
-
-    // Room
-    implementation ("androidx.room:room-runtime:2.4.1")
-    kapt ("androidx.room:room-compiler:2.4.1")
-
-    // Hilt
-    implementation ("com.google.dagger:hilt-android:2.40.5")
-    kapt ("com.google.dagger:hilt-compiler:2.40.5")
-
-    // Coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
-
-    // Work Manager
-    implementation ("androidx.work:work-runtime-ktx:2.7.1")
-
-    // ViewPager2
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
-
-    // Lifecycle
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
-
-    // Activity & Fragment KTX
-    implementation ("androidx.fragment:fragment-ktx:1.4.1")
-    implementation ("androidx.activity:activity-ktx:1.4.0")
-    implementation ("androidx.appcompat:appcompat:1.4.1")
-
-    // Timber & Logger
-    implementation ("com.jakewharton.timber:timber:5.0.1")
-    implementation ("com.orhanobut:logger:2.2.0")
+    core()
+    appCompat()
+    materialDesign()
+    constraintLayout()
+    junit()
+    espresso()
+    navigation()
+    room()
+    hilt()
+    coroutine()
+    workManager()
+    viewPager()
+    lifecycle()
+    ktx()
+    timber()
+    logger()
 }
