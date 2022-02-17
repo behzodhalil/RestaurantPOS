62d99748c6f8c6746f4412213397581506b0f18d
behzod <behzoddev@gmail.com>
2022-02-17 11:24:11 +0900

    Add UpdateMenuInteractor

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/menu/UpdateMenuInteractorImpl.kt
@@ -0,0 +1,14 @@
+package uz.behzoddev.restaurantpos.domain.interactors.menu
+
+import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
+import uz.behzoddev.restaurantpos.di.annotations.RepositoryBinds
+import uz.behzoddev.restaurantpos.domain.repositories.MenuRepository
+import javax.inject.Inject
+
+class UpdateMenuInteractorImpl @Inject constructor(
+    @RepositoryBinds private val menuRepository: MenuRepository
+) : UpdateMenuInteractor {
+    override suspend fun invoke(foodMenu: FoodMenu): Long {
+        return menuRepository.updateMenu(foodMenu)
+    }
+}
