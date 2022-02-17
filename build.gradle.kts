cf38066cc74394b021af307109349c029e54c8e8
behzod <behzoddev@gmail.com>
2022-02-17 14:19:05 +0900

    Update gradle with DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,14 @@
 buildscript {
 
     dependencies {
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
-        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
+        gradle()
     }
 }
 
 plugins {
-    id ("com.android.application") version ("7.1.0") apply (false)
-    id ("com.android.library") version ("7.1.0") apply (false)
-    id ("org.jetbrains.kotlin.android") version ("1.6.10") apply (false)
+    id ("com.android.application") version (Versions.applicationVersion) apply (false)
+    id ("com.android.library") version (Versions.libraryVersion) apply (false)
+    id ("org.jetbrains.kotlin.android") version (Versions.kotlinVersion) apply (false)
 }
 
 tasks.register("clean", Delete::class){
