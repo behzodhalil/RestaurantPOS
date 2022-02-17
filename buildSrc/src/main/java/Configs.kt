203e6ad27e4cd9da328fd673fccc13c9f83bb475
behzod <behzoddev@gmail.com>
2022-02-17 14:18:35 +0900

    Add configs to buildSrc

---
+++ buildSrc/src/main/java/Configs.kt
@@ -0,0 +1,8 @@
+object Configs {
+    const val applicationIdName = "uz.behzoddev.restaurantpos"
+    const val minSdkVersion = 21
+    const val compileSdkVersion = 31
+    const val targetSdkVersion = 31
+    const val versionCodeVersion = 1
+    const val versionName = "1.0"
+}
