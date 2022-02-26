14759734b97df32e21ce02155a2579a93f020047
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-26 10:46:33 +0900

    Update InsertStoreInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/store/InsertStoreInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/store/InsertStoreInteractorImpl.kt
@@ -1,13 +1,18 @@
 package uz.behzoddev.restaurantpos.domain.interactors.store
 
+import kotlinx.coroutines.withContext
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodStore
 import uz.behzoddev.restaurantpos.domain.repositories.StoreRepository
 import javax.inject.Inject
 
 class InsertStoreInteractorImpl @Inject constructor(
-    private val storeRepository: StoreRepository
+    private val storeRepository: StoreRepository,
+    private val dispatcherProviders: DispatcherProviders
 ) : InsertStoreInteractor {
     override suspend fun invoke(foodStore: FoodStore): Long {
-        return storeRepository.insertStore(foodStore)
+        return withContext(dispatcherProviders.io) {
+            storeRepository.insertStore(foodStore)
+        }
     }
 }
