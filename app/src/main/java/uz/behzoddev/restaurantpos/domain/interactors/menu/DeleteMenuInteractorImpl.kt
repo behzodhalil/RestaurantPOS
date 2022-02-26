947ff01cfbbd0cc45526bc125fdb01af850040cc
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-26 11:03:52 +0900

    Update DeleteMenuInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/menu/DeleteMenuInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/menu/DeleteMenuInteractorImpl.kt
@@ -1,14 +1,19 @@
 package uz.behzoddev.restaurantpos.domain.interactors.menu
 
+import kotlinx.coroutines.withContext
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
 import uz.behzoddev.restaurantpos.di.annotations.RepositoryBinds
 import uz.behzoddev.restaurantpos.domain.repositories.MenuRepository
 import javax.inject.Inject
 
 class DeleteMenuInteractorImpl @Inject constructor(
-    @RepositoryBinds private val menuRepository: MenuRepository
+    @RepositoryBinds private val menuRepository: MenuRepository,
+    private val dispatcherProviders: DispatcherProviders
 ): DeleteMenuInteractor {
     override suspend fun invoke(foodMenu: FoodMenu): Int {
-        return menuRepository.deleteMenu(foodMenu)
+        return withContext(dispatcherProviders.io) {
+            menuRepository.deleteMenu(foodMenu)
+        }
     }
 }
