a9cea498e66c698c361f597ae008d131594c8ca9
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-26 10:48:45 +0900

    Update FetchAllFoodItemsByNameInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchAllFoodItemsByNameInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchAllFoodItemsByNameInteractorImpl.kt
@@ -1,14 +1,17 @@
 package uz.behzoddev.restaurantpos.domain.interactors.item
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.domain.repositories.ItemRepository
 import javax.inject.Inject
 
 class FetchAllFoodItemsByNameInteractorImpl @Inject constructor(
-    private val itemRepository: ItemRepository
+    private val itemRepository: ItemRepository,
+    private val dispatcherProvider: DispatcherProviders
 ) : FetchAllFoodItemsByNameInteractor {
     override fun invoke(): Flow<List<FoodItem>> {
-        return itemRepository.fetchAllFoodItemsByName()
+        return itemRepository.fetchAllFoodItemsByName().flowOn(dispatcherProvider.io)
     }
 }
