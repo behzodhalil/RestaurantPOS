a534cc6ab204f0612923667c6476b628667f85ae
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-26 11:04:06 +0900

    Update DeleteStoreInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/store/DeleteStoreInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/store/DeleteStoreInteractorImpl.kt
@@ -1,13 +1,18 @@
 package uz.behzoddev.restaurantpos.domain.interactors.store
 
+import kotlinx.coroutines.withContext
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodStore
 import uz.behzoddev.restaurantpos.domain.repositories.StoreRepository
 import javax.inject.Inject
 
 class DeleteStoreInteractorImpl @Inject constructor(
-    private val storeRepository: StoreRepository
+    private val storeRepository: StoreRepository,
+    private val dispatcherProviders: DispatcherProviders
 ) : DeleteStoreInteractor {
     override suspend fun invoke(foodStore: FoodStore): Int {
-        return storeRepository.deleteStore(foodStore)
+        return withContext(dispatcherProviders.io) {
+            storeRepository.deleteStore(foodStore)
+        }
     }
 }
