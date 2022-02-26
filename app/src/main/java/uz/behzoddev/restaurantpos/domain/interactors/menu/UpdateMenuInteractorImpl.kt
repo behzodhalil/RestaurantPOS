68802ecf161f5a1f70899cc900ea675056984962
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-26 10:45:19 +0900

    Update UpdateMenuInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/menu/UpdateMenuInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/menu/UpdateMenuInteractorImpl.kt
@@ -1,14 +1,19 @@
 package uz.behzoddev.restaurantpos.domain.interactors.menu
 
+import kotlinx.coroutines.withContext
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
 import uz.behzoddev.restaurantpos.di.annotations.RepositoryBinds
 import uz.behzoddev.restaurantpos.domain.repositories.MenuRepository
 import javax.inject.Inject
 
 class UpdateMenuInteractorImpl @Inject constructor(
-    @RepositoryBinds private val menuRepository: MenuRepository
+    @RepositoryBinds private val menuRepository: MenuRepository,
+    private val dispatcherProviders: DispatcherProviders
 ) : UpdateMenuInteractor {
     override suspend fun invoke(foodMenu: FoodMenu): Int {
-        return menuRepository.updateMenu(foodMenu)
+        return withContext(dispatcherProviders.io) {
+            menuRepository.updateMenu(foodMenu)
+        }
     }
 }
