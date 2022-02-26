2dead7908df1ef53bf9f5cd6c603fc3a1ebbafc3
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-26 10:50:16 +0900

    Update FetchAllMenusInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/menu/FetchAllMenusInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/menu/FetchAllMenusInteractorImpl.kt
@@ -1,15 +1,19 @@
 package uz.behzoddev.restaurantpos.domain.interactors.menu
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
 import uz.behzoddev.restaurantpos.di.annotations.RepositoryBinds
 import uz.behzoddev.restaurantpos.domain.repositories.MenuRepository
 import javax.inject.Inject
 
 class FetchAllMenusInteractorImpl @Inject constructor(
-    @RepositoryBinds private val menuRepository: MenuRepository
+    @RepositoryBinds private val menuRepository: MenuRepository,
+    private val dispatcherProvider: DispatcherProviders
 ) : FetchAllMenusInteractor {
     override fun invoke(): Flow<List<FoodMenu>> {
-        TODO("Not yet implemented")
+        return menuRepository.fetchAllMenus().flowOn(dispatcherProvider.io)
     }
 }
