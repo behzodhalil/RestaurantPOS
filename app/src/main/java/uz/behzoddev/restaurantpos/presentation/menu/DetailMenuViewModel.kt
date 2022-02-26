cb240a28b6e0e4d79bf9e8f328be587777f27d0c
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-26 11:04:33 +0900

    Add DetailMenuViewModel.kt

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/presentation/menu/DetailMenuViewModel.kt
@@ -0,0 +1,9 @@
+package uz.behzoddev.restaurantpos.presentation.menu
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class DetailMenuViewModel : ViewModel() {
+
+}
