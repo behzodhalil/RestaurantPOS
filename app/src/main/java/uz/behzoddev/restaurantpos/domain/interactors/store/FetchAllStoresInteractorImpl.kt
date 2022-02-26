90506a87abffbc392c3e775eea418696c6ef1489
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-26 10:50:44 +0900

    Update FetchAllStoresInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/store/FetchAllStoresInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/store/FetchAllStoresInteractorImpl.kt
@@ -1,14 +1,17 @@
 package uz.behzoddev.restaurantpos.domain.interactors.store
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodStore
 import uz.behzoddev.restaurantpos.domain.repositories.StoreRepository
 import javax.inject.Inject
 
 class FetchAllStoresInteractorImpl @Inject constructor(
-   private val storeRepository: StoreRepository
+   private val storeRepository: StoreRepository,
+   private val dispatcherProviders: DispatcherProviders
 ) : FetchAllStoresInteractor {
     override fun invoke(): Flow<List<FoodStore>> {
-        return storeRepository.fetchAllStores()
+        return storeRepository.fetchAllStores().flowOn(dispatcherProviders.io)
     }
 }
