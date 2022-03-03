0b175cff209a7226d15e1fc7c2d561b424e537b9
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 15:44:56 +0900

    Update FoodMenu.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodMenu.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodMenu.kt
@@ -7,9 +7,11 @@ import uz.behzoddev.restaurantpos.common.local.RoomContract
 
 @Entity(tableName = RoomContract.FoodMenuContract.FOOD_MENU_TABLE)
 data class FoodMenu(
+    @ColumnInfo(name = RoomContract.FoodMenuContract.FOOD_MENU_NAME)
+    val foodMenuCategoryName: String,
+    @ColumnInfo(name = RoomContract.FoodMenuContract.FOOD_MENU_DESC_NAME)
+    var foodMenuCategoryDesc: String,
     @ColumnInfo(name = RoomContract.FoodMenuContract.FOOD_MENU_ID)
     @PrimaryKey(autoGenerate = true)
-    val foodMenuId: Int,
-    @ColumnInfo(name = RoomContract.FoodMenuContract.FOOD_MENU_NAME)
-    val foodMenuName: String
+    val foodMenuId: Int = 0,
 )
