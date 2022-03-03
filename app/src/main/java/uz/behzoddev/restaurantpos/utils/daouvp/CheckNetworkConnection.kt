3d437ed3cadeaeae6d2ab7a2bbdb2d266e6cc09e
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 10:39:35 +0900

    Add CheckNetworkConnection.kt

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/utils/daouvp/CheckNetworkConnection.kt
@@ -0,0 +1,35 @@
+package uz.behzoddev.restaurantpos.utils.daouvp
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+
+class CheckNetworkConnection(context: Context) : ConnectivityManager.NetworkCallback() {
+
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val networkRequest =
+        NetworkRequest.Builder().addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            .addTransportType(
+                NetworkCapabilities.TRANSPORT_WIFI
+            ).addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET).build()
+
+    fun register() {
+        connectivityManager.registerNetworkCallback(networkRequest, this)
+    }
+
+    fun unregister() {
+        connectivityManager.unregisterNetworkCallback(this)
+    }
+
+    override fun onAvailable(network: Network) {
+        super.onAvailable(network)
+    }
+
+    override fun onLost(network: Network) {
+        super.onLost(network)
+    }
+
+}
