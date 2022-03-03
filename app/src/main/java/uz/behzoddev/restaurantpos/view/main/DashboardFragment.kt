51d1daba30e23be0c10f39156a8cf84bdaf9e710
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 10:43:20 +0900

    Update DashboardFragment.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/main/DashboardFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/main/DashboardFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Spinner
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import uz.behzoddev.restaurantpos.R
 import uz.behzoddev.restaurantpos.common.base.BaseFragment
 import uz.behzoddev.restaurantpos.databinding.FragmentDashboardBinding
 
@@ -23,5 +25,31 @@ class DashboardFragment : BaseFragment<FragmentDashboardBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onNavigationToSettings()
+        onNavigateToStore()
+        onNavigateToTakeOut()
+    }
+
+    private fun onNavigationToSettings() = with(binding) {
+        var clickCounter = 0
+        rootLayout.setOnClickListener {
+            clickCounter +=1
+            if(clickCounter == 5) {
+                clickCounter = 0
+                findNavController().navigate(R.id.actionMainFragmentToMenuSettingsFragment)
+            }
+        }
+    }
+
+    private fun onNavigateToStore() = with(binding) {
+        btnOrderStore.setOnClickListener {
+            findNavController().navigate(R.id.actionMainFragmentToOrderStoreFragment)
+        }
+    }
+
+    private fun onNavigateToTakeOut() = with(binding) {
+        btnOrderTakeOut.setOnClickListener {
+            findNavController().navigate(R.id.actionMainFragmentToOrderTakeOutFragment)
+        }
     }
 }
