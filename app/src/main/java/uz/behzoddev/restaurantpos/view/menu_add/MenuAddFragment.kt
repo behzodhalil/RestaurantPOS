294bff627b7f545a8074154ed380e8c7ecc2a0c7
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 10:43:53 +0900

    Update MenuAddFragment.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/menu_add/MenuAddFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/menu_add/MenuAddFragment.kt
@@ -6,15 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
 import uz.behzoddev.restaurantpos.common.base.BaseFragment
-import uz.behzoddev.restaurantpos.databinding.FragmentAddMenuBinding
+import uz.behzoddev.restaurantpos.databinding.FragmentAddItemBinding
 
 @AndroidEntryPoint
-class MenuAddFragment : BaseFragment<FragmentAddMenuBinding>() {
+class MenuAddFragment : BaseFragment<FragmentAddItemBinding>() {
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentAddMenuBinding {
-        return FragmentAddMenuBinding.inflate(inflater,container,false)
+    ): FragmentAddItemBinding {
+        return FragmentAddItemBinding.inflate(inflater,container,false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
