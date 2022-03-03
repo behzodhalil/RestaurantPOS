48800143a4909b76d8013bf6f232497c340b38b8
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 15:47:05 +0900

    Update MenuSettingsFragment.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/menu_settings/MenuSettingsFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/menu_settings/MenuSettingsFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import uz.behzoddev.restaurantpos.R
 import uz.behzoddev.restaurantpos.common.base.BaseFragment
 import uz.behzoddev.restaurantpos.databinding.FragmentMenuSettingBinding
 
@@ -20,5 +22,12 @@ class MenuSettingsFragment : BaseFragment<FragmentMenuSettingBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onNavigateMenuManagement()
+    }
+
+    private fun onNavigateMenuManagement() = with(binding) {
+        btnMenuManagement.setOnClickListener {
+            findNavController().navigate(R.id.actionMenuSettingsFragmentToMenuManagementFragment)
+        }
     }
 }
