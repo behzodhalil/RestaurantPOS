0898628238a33e53ed0485506d5f8ad3be016e76
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 15:48:10 +0900

    Update MenuManagementAdapter.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/menu_management/MenuManagementAdapter.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/menu_management/MenuManagementAdapter.kt
@@ -1,4 +1,15 @@
 package uz.behzoddev.restaurantpos.view.menu_management
 
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import uz.behzoddev.restaurantpos.databinding.ItemExpandMenuContentBinding
+
 class MenuManagementAdapter {
+
+    sealed class MenuViewHolder(binding: ViewBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        class ItemContentViewHolder(binding: ItemExpandMenuContentBinding) : MenuViewHolder(binding) {
+
+        }
+    }
 }
