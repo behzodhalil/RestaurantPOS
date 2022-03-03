f95f2dc01596f3bf104c54f59b60ec6966f7ac2b
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 10:43:41 +0900

    Update MainActivity.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/MainActivity.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navHostFragment: NavHostFragment
 
+    companion object {
+        const val LAUNCH_MODE = 0
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
