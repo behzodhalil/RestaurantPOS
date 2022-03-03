a8ba93738f5a90ad7b960ef481dba7ccbeaf92aa
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 10:42:38 +0900

    Update FoodMenuDao.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/dao/FoodMenuDao.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/dao/FoodMenuDao.kt
@@ -19,6 +19,6 @@ interface FoodMenuDao {
     suspend fun delete(foodMenu: FoodMenu): Int
     @Query("SELECT * FROM food_menu_table")
     fun fetchAllMenus() : Flow<List<FoodMenu>>
-    @Query("SELECT * FROM food_menu_table ORDER BY food_menu_id =:foodId")
+    @Query("SELECT * FROM food_menu_table ORDER BY food_menu_id =:foodMenuId")
     fun fetchMenuById(foodMenuId: Int) : Flow<FoodMenu>
 }
