b87bb52ba6a11131883208cd977b9bd5aa5e56c4
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-03 10:44:03 +0900

    Update OrderStoreFragment.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/order_store/OrderStoreFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/order_store/OrderStoreFragment.kt
@@ -1,7 +1,24 @@
 package uz.behzoddev.restaurantpos.view.order_store
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
+import uz.behzoddev.restaurantpos.common.base.BaseFragment
+import uz.behzoddev.restaurantpos.databinding.FragmentOrderStoreBinding
 
 @AndroidEntryPoint
-class OrderStoreFragment {
+class OrderStoreFragment : BaseFragment<FragmentOrderStoreBinding>() {
+
+    override fun getViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentOrderStoreBinding {
+        return FragmentOrderStoreBinding.inflate(inflater,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
