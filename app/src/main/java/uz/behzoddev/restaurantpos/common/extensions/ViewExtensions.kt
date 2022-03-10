58f481421aea6134e776746119c232fd121b8ed7
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-10 10:06:57 +0900

    Update ViewExtensions.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/common/extensions/ViewExtensions.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/common/extensions/ViewExtensions.kt
@@ -1,10 +1,11 @@
 package uz.behzoddev.restaurantpos.common.extensions
 
+import android.graphics.drawable.Drawable
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
-/**
+/*
  * The Fragment toast extension functions
   */
 fun Fragment.showToastLong(message: String) {
@@ -14,12 +15,13 @@ fun Fragment.showToastShort(message : String) {
     Toast.makeText(activity,message,Toast.LENGTH_SHORT).show()
 }
 
-/**
+/*
  * The Activity toast extension functions
  */
 fun AppCompatActivity.showToastLong(message: String) {
     Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
 }
+
 fun AppCompatActivity.showToastShort(message: String) {
     Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
 }
