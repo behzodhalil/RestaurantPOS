8e1f978235ab47499fac0096b80b21a90c71ec07
behzod <behzoddev@gmail.com>
2022-03-10 13:50:06 +0900

    Update MainActivity

--- app/src/main/java/uz/behzoddev/restaurantpos/view/MainActivity.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/MainActivity.kt
@@ -1,10 +1,10 @@
 package uz.behzoddev.restaurantpos.view
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 import uz.behzoddev.restaurantpos.R
 import uz.behzoddev.restaurantpos.common.extensions.debug
 import uz.behzoddev.restaurantpos.databinding.ActivityMainBinding
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navHostFragment: NavHostFragment
-
+    private lateinit var decorView: View
     companion object {
         const val LAUNCH_MODE = 0
     }
@@ -22,8 +22,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        hideSystemUI()
         debug { "onCreate() method is created" }
         setupNavigation()
+    }
+
+    private fun hideSystemUI() {
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        decorView = window.decorView
+        val uIOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        decorView.systemUiVisibility = uIOptions
+    // https://developer.android.com/training/system-ui/navigation.htm
+    // https://stackoverflow.com/questions/52379561/how-android-bottom-navigation-bar-hide-permanently-or-disable
     }
 
     private fun setupNavigation() {
