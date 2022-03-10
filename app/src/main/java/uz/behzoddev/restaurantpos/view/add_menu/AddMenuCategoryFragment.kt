a74933a1d0c6ba603e2ea18da159b8dbb53696c3
behzod <behzoddev@gmail.com>
2022-03-10 14:49:34 +0900

    Update AddMenuCategoryFragment.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/add_menu/AddMenuCategoryFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/add_menu/AddMenuCategoryFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import uz.behzoddev.restaurantpos.R
 import uz.behzoddev.restaurantpos.common.base.BaseFragment
+import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.databinding.FragmentAddItemBinding
 import uz.behzoddev.restaurantpos.presentation.menu.AddMenuViewModel
 
@@ -29,7 +32,19 @@ class AddMenuCategoryFragment : BaseFragment<FragmentAddItemBinding>() {
 
     private fun insertMenu() = with(binding) {
         btnSave.setOnClickListener {
-
+                addViewModel.insertMenu(fetchItems()).run {
+                    findNavController().navigate(R.id.actionAddMenuCategoryFragmentToMenuManagementFragment)
+                }
             }
         }
+
+    private fun fetchItems(): FoodItem = binding.layoutMenuAddItem.let {
+        val title = it.tieMenuName.text.toString()
+        val price = it.tieMenuPrice.text.toString().toLong()
+        val quantity = it.tieMenuQuantity.text.toString().toInt()
+        val category = it.actCategory.text.toString()
+        val image = it.insertPhoto.toString()
+
+        return@fetchItems FoodItem(title,price,quantity,category,image)
     }
+}
