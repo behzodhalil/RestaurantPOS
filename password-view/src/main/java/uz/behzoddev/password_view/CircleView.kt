62022aab709627d93fb4589ab653d36039052e13
behzod <behzoddev@gmail.com>
2022-03-10 16:37:05 +0900

    Update CircleView.kt

--- password-view/src/main/java/uz/behzoddev/password_view/CircleView.kt
+++ password-view/src/main/java/uz/behzoddev/password_view/CircleView.kt
@@ -7,71 +7,50 @@ import android.graphics.Paint
 import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.Nullable
+import kotlin.math.min
 
-internal class CircleView @JvmOverloads constructor(
-    context: Context,
-    attributeSet: AttributeSet? = null,
-    defStyleAttribute: Int = 0
-) : View(context,attributeSet,defStyleAttribute) {
+// https://github.com/hanks-zyh/PasscodeView/blob/master/passcodeview/src/main/res/layout/layout_passcode_view.xml
+internal class CircleView : View {
+    private var mPaint: Paint? = null
+    private var color = Color.BLACK
 
-    private val outLinePaint = Paint(ANTI_ALIAS_FLAG).apply {
-        color = resources.getColor(R.color.dodger_blue)
-        strokeWidth = 3f
-        style = Paint.Style.STROKE
+    constructor(context: Context?) : super(context) {
+        init()
     }
 
-    private val fillCirclePaint = Paint(ANTI_ALIAS_FLAG).apply {
-        color = Color.WHITE
-        style = Paint.Style.FILL
+    constructor(context: Context?, @Nullable attrs: AttributeSet?) : super(context, attrs) {
+        init()
     }
 
-    private val fillAndStrokeCirclePaint = Paint(ANTI_ALIAS_FLAG).apply {
-        color = Color.BLACK
-        style = Paint.Style.FILL_AND_STROKE
-    }
-    private var radius = 56f
-
-    private var progress = 0.0f
-    set(value) {
-        field = value
+    constructor(context: Context?, @Nullable attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val width = ((radius * 2) + (outLinePaint.strokeWidth)).toInt()
-        val height = ((radius * 2) + (outLinePaint.strokeWidth)).toInt()
-        setMeasuredDimension(width,height)
+    private fun init() {
+        mPaint = Paint(ANTI_ALIAS_FLAG)
     }
 
-    override fun onDraw(canvas: Canvas?) {
-
-        val halfOutLineStrokeWidth = outLinePaint.strokeWidth / 2
-
-        canvas?.drawCircle(
-            radius + halfOutLineStrokeWidth,
-            radius + halfOutLineStrokeWidth,
-            lerp(radius - halfOutLineStrokeWidth,0f,progress),
-            fillCirclePaint
-        )
-
-        canvas?.drawCircle(
-            radius + halfOutLineStrokeWidth,
-            radius + halfOutLineStrokeWidth,
-            lerp(radius, 0f, progress),
-            outLinePaint
-        )
-
-        // fill and stroke circle
-        canvas?.drawCircle(
-            radius + halfOutLineStrokeWidth,
-            radius + halfOutLineStrokeWidth,
-            lerp(0f, radius + halfOutLineStrokeWidth, progress),
-            fillAndStrokeCirclePaint
-        )
+    fun setColor(color: Int) {
+        this.color = color
+        invalidate()
     }
-    /*
-     * Linearly interpolate between two values.
-     */
-    private fun lerp(a: Float, b: Float, t: Float): Float {
-        return a + (b - a) * t
+
+    fun getColor(): Int {
+        return color
     }
+
+    override fun onDraw(canvas: Canvas) {
+        val width = (width - paddingLeft - paddingRight) * 0.5
+        val height = (height - paddingTop - paddingBottom) * 0.5
+        val cx = (paddingLeft + width).toInt()
+        val cy = (paddingTop + height).toInt()
+        val radius = min(width, height).toInt()
+        mPaint!!.color = color
+        canvas.drawCircle(cx.toFloat(), cy.toFloat(), radius.toFloat(), mPaint!!)
     }
+}
