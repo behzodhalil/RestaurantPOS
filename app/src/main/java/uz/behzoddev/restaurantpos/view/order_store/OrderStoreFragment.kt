4e41f075a2ff89561bb9b457daac40b7aec26b14
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-17 10:01:46 +0900

    Add OrderStoreFragment.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/order_store/OrderStoreFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/order_store/OrderStoreFragment.kt
@@ -18,7 +18,4 @@ class OrderStoreFragment : BaseFragment<FragmentOrderStoreBinding>() {
         return FragmentOrderStoreBinding.inflate(inflater,container,false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 }
