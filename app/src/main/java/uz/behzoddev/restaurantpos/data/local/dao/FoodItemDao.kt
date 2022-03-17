16807e4a809a0f3437e3e9d410b87929eced7a80
behzod <behzoddev@gmail.com>
2022-03-17 17:39:58 +0900

    Update FoodItemDao.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/dao/FoodItemDao.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/dao/FoodItemDao.kt
@@ -3,8 +3,9 @@ package uz.behzoddev.restaurantpos.data.local.dao
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
+import uz.behzoddev.restaurantpos.utils.*
 
-/**
+/*
 * Provides access to read/write operations on the food item table.
 * Used by the data source to format query, insert, delete results for use in the UI.
 */
@@ -21,4 +22,20 @@ interface FoodItemDao {
     fun fetchAllFoodItems(): Flow<List<FoodItem>>
     @Query("SELECT * FROM food_item_table ORDER BY food_item_name")
     fun fetchAllFoodItemsByName(): Flow<List<FoodItem>>
+    @Query("SELECT * FROM food_item_table WHERE food_item_category = '$COLD_BREW_COFFEE' ORDER BY food_item_name")
+    fun fetchByColdBrew(): Flow<List<FoodItem>>
+    @Query("SELECT * FROM food_item_table WHERE food_item_category = '$BROOD_COFFEE' ORDER BY food_item_name")
+    fun fetchByBrood(): Flow<List<FoodItem>>
+    @Query("SELECT * FROM food_item_table WHERE food_item_category = '$ESPRESSO' ORDER BY food_item_category")
+    fun fetchByEspresso(): Flow<List<FoodItem>>
+    @Query("SELECT * FROM food_item_table WHERE food_item_category = '$FRAPPUCCINO' ORDER BY food_item_category")
+    fun fetchByFrappucino(): Flow<List<FoodItem>>
+    @Query("SELECT *FROM food_item_table WHERE food_item_category = '$BLENDED' ORDER BY food_item_name")
+    fun fetchByBlended(): Flow<List<FoodItem>>
+    @Query("SELECT * FROM food_item_table WHERE food_item_category = '$STARBUCKS_PHYSIO' ORDER BY food_item_name ")
+    fun fetchByPhysio(): Flow<List<FoodItem>>
+    @Query("SELECT * FROM food_item_table WHERE food_item_category = '$TEA' ORDER BY food_item_name ")
+    fun fetchByTea(): Flow<List<FoodItem>>
+    @Query("SELECT * FROM food_item_table WHERE food_item_category = '$JUICE' ORDER BY food_item_name ")
+    fun fetchByJuice(): Flow<List<FoodItem>>
 }
