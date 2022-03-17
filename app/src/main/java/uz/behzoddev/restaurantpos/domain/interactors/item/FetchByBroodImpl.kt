0471899841674e903196a2221c446feb2d21ac63
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-17 21:59:18 +0900

    Update FetchByBrood.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByBroodImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByBroodImpl.kt
@@ -1,4 +1,14 @@
 package uz.behzoddev.restaurantpos.domain.interactors.item
 
-class FetchByBroodImpl {
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.models.FoodItem
+import uz.behzoddev.restaurantpos.domain.repositories.ItemRepository
+import javax.inject.Inject
+
+class FetchByBroodImpl @Inject constructor(
+    private val itemRepository: ItemRepository
+) : FetchByBrood {
+    override fun invoke(): Flow<List<FoodItem>> {
+        return itemRepository.fetchByBrood()
+    }
 }
