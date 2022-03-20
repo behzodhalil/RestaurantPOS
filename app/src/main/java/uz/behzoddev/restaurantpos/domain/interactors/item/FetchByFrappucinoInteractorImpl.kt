027c6e7b7c132f43ad962da47544514e0bb65f37
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-20 21:02:59 +0900

    Update FetchByFrappucinoInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByFrappucinoInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByFrappucinoInteractorImpl.kt
@@ -1,15 +1,18 @@
 package uz.behzoddev.restaurantpos.domain.interactors.item
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.domain.repositories.ItemRepository
 import javax.inject.Inject
 
 class FetchByFrappucinoInteractorImpl @Inject constructor(
-    private val itemRepository: ItemRepository
+    private val itemRepository: ItemRepository,
+    private val dispatcherProviders: DispatcherProviders
 )  : FetchByFrappucinoInteractor {
 
     override fun invoke(): Flow<List<FoodItem>> {
-        return itemRepository.fetchByFrappucino()
+        return itemRepository.fetchByFrappucino().flowOn(dispatcherProviders.io)
     }
 }
