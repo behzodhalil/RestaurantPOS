f88523ceaf259394e53d1e0bc3c059d7bb0b02ba
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-20 18:19:09 +0900

    Refactor Constants.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/utils/Constants.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/utils/Constants.kt
@@ -1,11 +1,25 @@
 package uz.behzoddev.restaurantpos.utils
 
+object CoffeeCategory {
+    const val COLD_BREW_COFFEE = "콜드브루 커피"
+    const val BROOD_COFFEE = "브루드 커피"
+    const val ESPRESSO = "에스프레소"
+    const val FRAPPUCCINO = "프라푸치노"
+    const val BLENDED = "블렌디드"
+    const val STARBUCKS_PHYSIO = "스타벅스 피지오"
+    const val TEA = "티(타비나)"
+    const val JUICE = "주스"
 
-const val COLD_BREW_COFFEE = "콜드브루 커피"
-const val BROOD_COFFEE = "브루드 커피"
-const val ESPRESSO = "에스프레소"
-const val FRAPPUCCINO = "프라푸치노"
-const val BLENDED = "블렌디드"
-const val STARBUCKS_PHYSIO = "스타벅스 피지오"
-const val TEA = "티(타비나)"
-const val JUICE = "주스"
+    val coffeeCategory = listOf(
+        "콜드브루 커피",
+        "브루드 커피",
+        "에스프레소",
+        "프라푸치노",
+        "블렌디드",
+        "스타벅스 피지오",
+        "티(타비나)",
+        "주스"
+    )
+}
+
+
