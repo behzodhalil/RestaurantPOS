be8a78ee2eaae6b72d1df0f3bb2935c516116946
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-20 21:02:47 +0900

    Update FetchByEspressoInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByEspressoInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByEspressoInteractorImpl.kt
@@ -1,15 +1,18 @@
 package uz.behzoddev.restaurantpos.domain.interactors.item
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.domain.repositories.ItemRepository
 import javax.inject.Inject
 
 class FetchByEspressoInteractorImpl @Inject constructor(
-    private val itemRepository: ItemRepository
+    private val itemRepository: ItemRepository,
+    private val dispatcherProviders: DispatcherProviders
 ) : FetchByEspressoInteractor {
 
     override fun invoke(): Flow<List<FoodItem>> {
-        return itemRepository.fetchByEspresso()
+        return itemRepository.fetchByEspresso().flowOn(dispatcherProviders.io)
     }
 }
