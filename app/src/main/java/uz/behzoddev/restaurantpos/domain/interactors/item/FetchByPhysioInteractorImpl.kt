eb77e0ccbb9ead494e143b59b75be20130668026
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-20 21:03:22 +0900

    Update FetchByPhysioInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByPhysioInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByPhysioInteractorImpl.kt
@@ -1,15 +1,18 @@
 package uz.behzoddev.restaurantpos.domain.interactors.item
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.domain.repositories.ItemRepository
 import javax.inject.Inject
 
 class FetchByPhysioInteractorImpl @Inject constructor(
-    private val itemRepository: ItemRepository
+    private val itemRepository: ItemRepository,
+    private val dispatcherProviders: DispatcherProviders
 ) : FetchByPhysioInteractor {
 
     override fun invoke(): Flow<List<FoodItem>> {
-        return itemRepository.fetchByPhysio()
+        return itemRepository.fetchByPhysio().flowOn(dispatcherProviders.io)
     }
 }
