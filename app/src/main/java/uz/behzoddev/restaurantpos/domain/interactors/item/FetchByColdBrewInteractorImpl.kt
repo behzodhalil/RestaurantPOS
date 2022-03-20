3292c4205b955ad1b37cbb96ee4e5ad5545b4963
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-20 21:02:36 +0900

    Update FetchByColdBrewInteractorImpl.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByColdBrewInteractorImpl.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByColdBrewInteractorImpl.kt
@@ -1,14 +1,18 @@
 package uz.behzoddev.restaurantpos.domain.interactors.item
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.domain.repositories.ItemRepository
 import javax.inject.Inject
 
 class FetchByColdBrewInteractorImpl @Inject constructor(
-    private val itemRepository: ItemRepository
+    private val itemRepository: ItemRepository,
+    private val dispatcherProviders: DispatcherProviders
 ) : FetchByColdBrewInteractor {
+
     override fun invoke(): Flow<List<FoodItem>> {
-        return itemRepository.fetchByColdBrew()
+        return itemRepository.fetchByColdBrew().flowOn(dispatcherProviders.io)
     }
 }
