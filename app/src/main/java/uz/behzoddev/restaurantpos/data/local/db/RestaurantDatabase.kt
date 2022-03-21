e64a20940ab6856c355e39b2422265fb72ff5ac5
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-21 19:57:56 +0900

    Add migrations to RestaurantDatabase.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/db/RestaurantDatabase.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/db/RestaurantDatabase.kt
@@ -1,6 +1,7 @@
 package uz.behzoddev.restaurantpos.data.local.db
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -13,28 +14,34 @@ import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
 import uz.behzoddev.restaurantpos.data.local.models.FoodStore
 
 @Database(
-    entities = [FoodStore::class,FoodMenu::class,FoodItem::class],
-    version = 1,
-    exportSchema = false
+    entities = [FoodStore::class, FoodMenu::class, FoodItem::class],
+    version = 3,
+    autoMigrations = [
+        AutoMigration(from = 2, to = 3)
+    ],
+    exportSchema = true
 )
 abstract class RestaurantDatabase : RoomDatabase() {
 
-    abstract fun getItemDao() : FoodItemDao
-    abstract fun getMenuDao() : FoodMenuDao
-    abstract fun getStoreDao() : FoodStoreDao
+    abstract fun getItemDao(): FoodItemDao
+    abstract fun getMenuDao(): FoodMenuDao
+    abstract fun getStoreDao(): FoodStoreDao
 
     companion object {
         @Volatile
-        private var INSTANCE : RestaurantDatabase? = null
+        private var INSTANCE: RestaurantDatabase? = null
         private val LOCK = Any()
 
         operator fun invoke(context: Context) = INSTANCE ?: synchronized(LOCK) {
             INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
         }
+
         private fun buildDatabase(context: Context) = Room.databaseBuilder(
             context.applicationContext,
             RestaurantDatabase::class.java,
             RoomContract.DATABASE_NAME
-        ).build()
+        ).fallbackToDestructiveMigration().build()
+
     }
 }
+
