05a85d4ad97b09243a3dcbd878d0539214428c39
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-21 16:27:49 +0900

    Update MenuFragment.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/menu/MenuFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/menu/MenuFragment.kt
@@ -9,6 +9,7 @@ import uz.behzoddev.restaurantpos.R
 import uz.behzoddev.restaurantpos.common.base.BaseFragment
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.databinding.FragmentMenuBinding
+import uz.behzoddev.restaurantpos.utils.SliderTransformer
 import uz.behzoddev.restaurantpos.view.order_store.OrderStoreAdapter
 
 class MenuFragment : BaseFragment<FragmentMenuBinding>() {
@@ -22,18 +23,21 @@ class MenuFragment : BaseFragment<FragmentMenuBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.viewPager.adapter = MenuViewPagerAdapter(this)
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             tab.text = getTabTitle(position)
         }.attach()
+
     }
 
     private fun getTabTitle(position: Int): String? =  when(position) {
         COLD_BREW_COFFEE_INDEX -> getString(R.string.cold_brew_coffee)
-        // BROOD_COFFEE_INDEX -> getString(R.string.brood_coffee)
         ESPRESSO_INDEX -> getString(R.string.espresso)
         TEA_INDEX -> getString(R.string.tea)
-       // FRAPPUCCINO_INDEX -> getString(R.string.frappucino)
+        FRAPPUCCINO_INDEX -> getString(R.string.frappucino)
+        BROOD_INDEX -> getString(R.string.brood_coffee)
+        JUICE_INDEX -> getString(R.string.juice)
         // BLENDED_INDEX -> getString(R.string.blended)
         // STARBUCKS_PHYSIO_INDEX -> getString(R.string.starbucks_physio)
         else -> null
