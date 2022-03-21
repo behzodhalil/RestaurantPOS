90017b7cf185c43d64e768f0021c04b7a9d51c81
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-21 16:30:34 +0900

    Update FrappuccinoFragment.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/menu/FrappuccinoFragment.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/menu/FrappuccinoFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import uz.behzoddev.restaurantpos.common.base.BaseFragment
+import uz.behzoddev.restaurantpos.common.states.ItemState
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.databinding.FragmentFrappuccinoBinding
 import uz.behzoddev.restaurantpos.presentation.store.FrappuccinoViewModel
@@ -30,14 +32,34 @@ class FrappuccinoFragment : BaseFragment<FragmentFrappuccinoBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initFrappuccinoRecyclerView()
     }
 
-    private fun initFrappuccinoRecyclerView() {
-
+    private fun initFrappuccinoRecyclerView() = with(binding) {
+        frappuccinoRecyclerView.adapter = frappuccinoAdapter
+        // When you call that function, the size (with and height) of recycler view
+        // won’t change on the adapter insert/update/delete
+        frappuccinoRecyclerView.setHasFixedSize(true)
     }
 
     private fun observerFrappuccino() = lifecycleScope.launchWhenCreated {
+        frappuccinoViewModel.frappuccinoState.collect { result ->
+            when(result) {
+                is ItemState.Loading -> {
 
+                }
+                is ItemState.Empty -> {
+
+                }
+                is ItemState.Failure -> {
+
+                }
+                is ItemState.Success -> {
+                    fetchAllFrappuccino(result.data)
+                }
+            }
+
+        }
     }
     private fun fetchAllFrappuccino(list: List<FoodItem>) = frappuccinoAdapter.diffUtil.submitList(list)
 }
