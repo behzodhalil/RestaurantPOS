7ca4c244fd1dab6b93a93bd13a92bc3cc08790d3
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-22 19:55:33 +0900

    Add DetailMenuViewModel.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/presentation/menu/DetailMenuViewModel.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/presentation/menu/DetailMenuViewModel.kt
@@ -1,6 +1,33 @@
 package uz.behzoddev.restaurantpos.presentation.menu
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import uz.behzoddev.restaurantpos.common.states.DetailState
+import uz.behzoddev.restaurantpos.data.local.models.FoodItem
+import uz.behzoddev.restaurantpos.domain.interactors.item.FetchByIdInteractor
+import javax.inject.Inject
 
+@HiltViewModel
+class DetailMenuViewModel @Inject constructor(
+    private val fetchByIdInteractor: FetchByIdInteractor
+) : ViewModel() {
 
-class DetailMenuViewModel : ViewModel()
+    private var _detailState = MutableStateFlow<DetailState>(DetailState.Loading)
+    val detailState: StateFlow<DetailState> get() = _detailState
+
+    fun fetchById(id: Int) = viewModelScope.launch {
+        _detailState.value = DetailState.Loading
+        fetchByIdInteractor.invoke(id).collect { result: FoodItem? ->
+            if (result != null) {
+                _detailState.value = DetailState.Success(result)
+            } else {
+                _detailState.value = DetailState.Failure("Failure")
+            }
+        }
+        }
+}
