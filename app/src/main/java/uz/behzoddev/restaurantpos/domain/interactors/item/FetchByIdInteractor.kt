e603345109ff04feeeeca738752254af97b970ba
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-22 19:48:06 +0900

    Add FetchByIdInteractor.kt

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/item/FetchByIdInteractor.kt
@@ -0,0 +1,8 @@
+package uz.behzoddev.restaurantpos.domain.interactors.item
+
+import kotlinx.coroutines.flow.Flow
+import uz.behzoddev.restaurantpos.data.local.models.FoodItem
+
+interface FetchByIdInteractor {
+    operator fun invoke(id: Int): Flow<FoodItem>
+}
