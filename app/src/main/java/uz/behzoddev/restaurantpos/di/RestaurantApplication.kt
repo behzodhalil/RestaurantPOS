0d2c4e79dd700942b9adab7c9470f08f5dcf9581
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-27 21:07:16 +0900

    Update RestaurantApplication.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/di/RestaurantApplication.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/di/RestaurantApplication.kt
@@ -1,6 +1,10 @@
 package uz.behzoddev.restaurantpos.di
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.orhanobut.logger.AndroidLogAdapter
 import com.orhanobut.logger.FormatStrategy
 import com.orhanobut.logger.Logger
@@ -8,13 +12,18 @@ import com.orhanobut.logger.PrettyFormatStrategy
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import uz.behzoddev.restaurantpos.common.extensions.debug
+import uz.behzoddev.restaurantpos.data.worker.RestaurantWorker
+import javax.inject.Inject
 
 @HiltAndroidApp
-class RestaurantApplication : Application() {
+class RestaurantApplication : Application(),Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
 
     override fun onCreate() {
         super.onCreate()
-
+        // prePopulateDataWithWorkManager()
         val formatStrategy: FormatStrategy = PrettyFormatStrategy.newBuilder()
             .showThreadInfo(true)
             .methodCount(1)
@@ -31,5 +40,13 @@ class RestaurantApplication : Application() {
         })
 
         debug { "onCreate() method is created" }
+    }
+
+    private fun prePopulateDataWithWorkManager() {
+        val foodItemWork = OneTimeWorkRequestBuilder<RestaurantWorker>().build()
+        WorkManager.getInstance(applicationContext).beginWith(foodItemWork).enqueue()
+    }
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder().setWorkerFactory(workerFactory).build()
     }
 }
