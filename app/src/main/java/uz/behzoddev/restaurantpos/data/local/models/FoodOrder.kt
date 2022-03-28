fa9c709bb9fd3214819fff84dc0c02f15512e652
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-28 14:40:01 +0900

    Add FoodOrder.kt

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodOrder.kt
@@ -0,0 +1,42 @@
+package uz.behzoddev.restaurantpos.data.local.models
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import uz.behzoddev.restaurantpos.common.local.RoomContract
+import java.util.*
+import kotlin.collections.ArrayList
+
+@Parcelize
+@Entity(tableName = RoomContract.FoodOrderContract.FOOD_ORDER_TABLE)
+data class FoodOrder (
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_ITEMS)
+        var orderItems: ArrayList<FoodOrderItem>,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_TOTAL_AMOUNT)
+        var orderTotalAmount: Float,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_TOTAL_QUANTITY)
+        var orderTotalQuantity: Int,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_NUMBER)
+        var orderNumber: Int,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_IS_TAKEOUT)
+        var isTakeOut: Boolean,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_IS_PAID)
+        var isPaid: Boolean,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_TAX_RATED)
+        var orderTaxRated: Float,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_TIME_CREATED)
+        var timeCreated: Calendar = Calendar.getInstance(),
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_CUSTOMER)
+        var customer: String,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_IS_VALID)
+        var isValid: Boolean,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_IS_STORE)
+        var isStore: Boolean,
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_ID)
+        var orderId: Long
+): Parcelable {
+
+}
