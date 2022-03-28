79d00e4549e31933e5136f5d28f66eda1e89425e
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-28 15:09:37 +0900

    Update RestaurantDatabase.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/db/RestaurantDatabase.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/db/RestaurantDatabase.kt
@@ -1,10 +1,7 @@
 package uz.behzoddev.restaurantpos.data.local.db
 
 import android.content.Context
-import androidx.room.AutoMigration
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
@@ -12,25 +9,29 @@ import androidx.work.WorkManager
 import uz.behzoddev.restaurantpos.common.local.RoomContract
 import uz.behzoddev.restaurantpos.data.local.dao.FoodItemDao
 import uz.behzoddev.restaurantpos.data.local.dao.FoodMenuDao
+import uz.behzoddev.restaurantpos.data.local.dao.FoodOrderDao
 import uz.behzoddev.restaurantpos.data.local.dao.FoodStoreDao
 import uz.behzoddev.restaurantpos.data.local.models.FoodItem
 import uz.behzoddev.restaurantpos.data.local.models.FoodMenu
+import uz.behzoddev.restaurantpos.data.local.models.FoodOrder
 import uz.behzoddev.restaurantpos.data.local.models.FoodStore
 import uz.behzoddev.restaurantpos.data.worker.RestaurantWorker
 
 @Database(
-    entities = [FoodStore::class, FoodMenu::class, FoodItem::class],
-    version = 8,
+    entities = [FoodStore::class, FoodMenu::class, FoodItem::class,FoodOrder::class],
+    version = 11,
     autoMigrations = [
-        AutoMigration(from = 7, to = 8)
+        AutoMigration(from = 10, to = 11)
     ],
     exportSchema = true
 )
+@TypeConverters(Converter::class)
 abstract class RestaurantDatabase : RoomDatabase() {
 
     abstract fun getItemDao(): FoodItemDao
     abstract fun getMenuDao(): FoodMenuDao
     abstract fun getStoreDao(): FoodStoreDao
+    abstract fun getOrderDao(): FoodOrderDao
 
     companion object {
         @Volatile
