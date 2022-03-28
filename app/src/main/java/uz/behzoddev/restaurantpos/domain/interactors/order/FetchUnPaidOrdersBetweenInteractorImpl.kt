443aff1f38db813d9b9c3f97484e3bf0bf7e718a
Behzod Bozorboev <behzoddev@gmail.com>
2022-03-28 15:13:55 +0900

    Add FetchUnPaidOrdersBetweenInteractor.kt

---
+++ app/src/main/java/uz/behzoddev/restaurantpos/domain/interactors/order/FetchUnPaidOrdersBetweenInteractorImpl.kt
@@ -0,0 +1,17 @@
+package uz.behzoddev.restaurantpos.domain.interactors.order
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import uz.behzoddev.restaurantpos.common.coroutines.DispatcherProviders
+import uz.behzoddev.restaurantpos.data.local.models.FoodOrder
+import uz.behzoddev.restaurantpos.domain.repositories.OrderRepository
+import javax.inject.Inject
+
+class FetchUnPaidOrdersBetweenInteractorImpl @Inject constructor(
+    private val orderRepository: OrderRepository,
+    private val dispatcherProviders: DispatcherProviders
+) : FetchUnPaidOrdersBetweenInteractor {
+    override fun invoke(start: Long, end: Long): Flow<List<FoodOrder>> {
+        return orderRepository.fetchUnPaidOrdersBetween(start, end).flowOn(dispatcherProviders.io)
+    }
+}
