42cbd0e53dcda2b19b9bc5a67e63e777332de12f
Behzod Bozorboev <behzoddev@gmail.com>
2022-04-03 13:53:36 +0900

    Update FoodOrder.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodOrder.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/data/local/models/FoodOrder.kt
@@ -2,6 +2,7 @@ package uz.behzoddev.restaurantpos.data.local.models
 
 import android.os.Parcelable
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
@@ -12,12 +13,14 @@ import kotlin.collections.ArrayList
 @Parcelize
 @Entity(tableName = RoomContract.FoodOrderContract.FOOD_ORDER_TABLE)
 data class FoodOrder (
-        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_ITEMS)
-        var orderItems: ArrayList<FoodOrderItem>,
+        @Embedded
+        var orderItems: FoodItem,
         @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_TOTAL_AMOUNT)
         var orderTotalAmount: Float,
         @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_TOTAL_QUANTITY)
         var orderTotalQuantity: Int,
+        @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_QUANTITY)
+        var orderQuantity: Int,
         @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_NUMBER)
         var orderNumber: Int,
         @ColumnInfo(name = RoomContract.FoodOrderContract.FOOD_ORDER_IS_TAKEOUT)
