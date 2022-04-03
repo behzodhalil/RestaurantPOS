f77816fb34789c41ad32c688a3d494db23fbc9a2
Behzod Bozorboev <behzoddev@gmail.com>
2022-04-03 13:55:04 +0900

    Update MenuViewPagerAdapter.kt

--- app/src/main/java/uz/behzoddev/restaurantpos/view/menu/MenuViewPagerAdapter.kt
+++ app/src/main/java/uz/behzoddev/restaurantpos/view/menu/MenuViewPagerAdapter.kt
@@ -1,7 +1,11 @@
 package uz.behzoddev.restaurantpos.view.menu
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import uz.behzoddev.restaurantpos.common.extensions.debug
+import javax.inject.Inject
 
 const val COLD_BREW_COFFEE_INDEX = 0
 const val ESPRESSO_INDEX = 1
@@ -10,9 +14,9 @@ const val FRAPPUCCINO_INDEX = 3
 const val BROOD_INDEX = 4
 const val JUICE_INDEX = 5
 
-class MenuViewPagerAdapter(
-    fragments: Fragment
-) : FragmentStateAdapter(fragments) {
+class MenuViewPagerAdapter (
+    fragment: Fragment
+) : FragmentStateAdapter(fragment) {
 
 
     private val fragments: Map<Int, () -> Fragment> = mapOf(
@@ -41,6 +45,7 @@ class MenuViewPagerAdapter(
     }
 
     override fun createFragment(position: Int): Fragment {
+        debug { "ViewPagerAdapter" }
         return fragments[position]?.invoke() ?: throw IndexOutOfBoundsException()
     }
 }
